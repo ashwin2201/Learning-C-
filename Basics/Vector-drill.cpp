e9d9fe89f58f05eabd5a3b3c99c12278f60db56e
ashwin2201 <40682196+ashwin2201@users.noreply.github.com>
2020-06-06 20:41:03 +0800

    Update Vector-drill.cpp

--- Basics/Vector-drill.cpp
+++ Basics/Vector-drill.cpp
@@ -1,1 +1,23 @@
+#include "std_lib_facilities.h"
+
+int main () {
+
+  double num;
+  double double1;
+  double double2;
+  vector<double> doubles;
+
+  while (cin >> num) {
+    doubles.push_back(num);
+  }
+  double sum = 0;
+  
+   for (double x : doubles) {
+    sum += x;
+  }
+  sort(doubles);
+  cout << "The smallest so far: " << doubles[0] <<"\n";
+  cout << "The largest so far: " << doubles[doubles.size()-1] <<"\n";
+  cout <<"sum = " <<sum;
+}
 
