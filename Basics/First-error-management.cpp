d5bc34c668446623426c1a217200071769bb22df
ashwin2201 <40682196+ashwin2201@users.noreply.github.com>
2020-06-10 22:13:32 +0800

    Create First-error-management.cpp

---
+++ Basics/First-error-management.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+
+class Bad_area {
+
+};
+
+int area (int length, int width) {
+  if (length < 0 || width < 0) {
+    throw Bad_area{};
+  }
+  return length * width;
+}
+
+int main () {
+  try {
+    int x = -1;
+    int y = 2;
+    int z = 4;
+
+    int area1 = area(x, y);
+    int area2 = area(y, z);
+  }
+
+  catch (Bad_area) {
+    cout << "Oops! Bad arguments to area()\n";
+  }
+
+}
